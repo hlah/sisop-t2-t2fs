4c6062c37b427f59de5f215a9cea70228fd62220
Mateus Davi Simon <hlah_@hotmail.com>
2018-11-05 11:05:54 -0200

    Resolvido diferença de definição e declaração da variavel global 't2fs_cwd_path'

--- t2fs/src/t2fs_global_data.c
+++ t2fs/src/t2fs_global_data.c
@@ -5,7 +5,7 @@ int t2fs_initialized = 0;
 // buffer superbloco
 struct t2fs_superbloco t2fs_superbloco_info;
 // caminho do diretório atual
-char t2fs_cwd_path[256];
+char t2fs_cwd_path[MAXIMUM_PATH_LEN+1];
 // cluster do diretório atual
 unsigned int t2fs_cwd_cluster_num;
 
