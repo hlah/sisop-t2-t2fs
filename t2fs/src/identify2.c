f151a6bb511b9999e9f8b19edb32bbc82c91a5ae
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-11-12 00:06:27 +0000

    Adicionado identify2 (NÃO TESTADO)

--- t2fs/src/identify2.c
+++ t2fs/src/identify2.c
@@ -1,6 +1,8 @@
 #include "t2fs_internal.h"
 
+#include <string.h>
+
 int identify2(char* name, int size) {
-	t2fs_init();
-	return -1;
+	strncpy (name, "Gerente: Artur Galvao Hallberg - 00246759\nMembro 1: Mateus Davi Simon - 00209411", size);
+	return 0;
 }
