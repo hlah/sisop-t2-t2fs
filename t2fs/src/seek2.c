f7d6fd9b7f45f202c879e8615389c6a8812d76a1
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-11-13 22:30:09 +0000

    seek2 simplificado

--- t2fs/src/seek2.c
+++ t2fs/src/seek2.c
@@ -4,8 +4,8 @@ int seek2 (FILE2 handle, DWORD offset) {
 	t2fs_init();
 
     unsigned int sector_buffer [SECTOR_SIZE / 4];
-    unsigned int required_clusters;
-	unsigned int num_new_clusters;
+    // unsigned int required_clusters;
+	// unsigned int num_new_clusters;
 	unsigned int cluster_size = SECTOR_SIZE * t2fs_superbloco_info.SectorsPerCluster;
     unsigned int sector;
     unsigned int sector_offset;
@@ -25,11 +25,59 @@ int seek2 (FILE2 handle, DWORD offset) {
 		return -1;
 	}
 
-    // seta current_pojnter
-    if ((int) offset == -1) {
+	// checa se offset ultrapassa o tamanho arquivo
+	if( offset > t2fs_open_files[handle].file_record->bytesFileSize && (int) offset != -1) {
+		return -1;
+	}
+
+    // seta current_pojnter e verifica se é necessário alocar um novo cluster
+    if ((int) offset == -1 || offset == t2fs_open_files[handle].file_record->bytesFileSize) {
         t2fs_open_files[handle].current_pointer = t2fs_open_files[handle].file_record->bytesFileSize;
+
+        // aloca novo cluster se necessário
+        if ( (t2fs_open_files[handle].current_pointer / cluster_size) == 0 ) {
+
+            // encontra último cluster do arquivo
+            sector = t2fs_open_files[handle].file_record->firstCluster / (SECTOR_SIZE / 4);
+            sector_offset = t2fs_open_files[handle].file_record->firstCluster % (SECTOR_SIZE / 4);
+            for (i = 0; i < t2fs_open_files[handle].file_record->clustersFileSize; i++) {
+                if (read_sector(sector + t2fs_superbloco_info.pFATSectorStart, (unsigned char*) sector_buffer) != 0)
+                    return -1;
+                next_cluster = sector_buffer[sector_offset];
+                if (next_cluster != 0xFFFFFFFF) {
+                    sector = next_cluster / (SECTOR_SIZE / 4);
+                    sector_offset = next_cluster % (SECTOR_SIZE / 4);
+                }
+            }
+
+            // encontra cluster livre e associa ao arquivo
+            new_cluster = t2fs_get_free_cluster();
+            if ((int) new_cluster == -1)
+                return -1;
+            sector_buffer[sector_offset] = new_cluster;
+            if (write_sector(sector + t2fs_superbloco_info.pFATSectorStart, (unsigned char*) sector_buffer) != 0)
+                return -1;
+            sector = new_cluster / (SECTOR_SIZE / 4);
+            sector_offset = new_cluster % (SECTOR_SIZE / 4);
+            if (read_sector(sector + t2fs_superbloco_info.pFATSectorStart, (unsigned char*) sector_buffer) != 0)
+                return -1;
+            sector_buffer[sector_offset] = 0xFFFFFFFF;
+            if (write_sector(sector + t2fs_superbloco_info.pFATSectorStart, (unsigned char*) sector_buffer) != 0)
+                return -1;
+
+            // atualiza tamanho
+            t2fs_open_files[handle].file_record->clustersFileSize = t2fs_open_files[handle].file_record->clustersFileSize + 1;
+        }
+
+        return 0;
     }
-    else t2fs_open_files[handle].current_pointer = offset;
+
+    t2fs_open_files[handle].current_pointer = offset;
+
+    return 0;
+
+
+    /*
 
     // atualiza tamanho e verifica clusters se necessário
     if (t2fs_open_files[handle].current_pointer >= t2fs_open_files[handle].file_record->bytesFileSize) {
@@ -76,4 +124,7 @@ int seek2 (FILE2 handle, DWORD offset) {
     }
 
 	return 0;
+
+	*/
+
 }
