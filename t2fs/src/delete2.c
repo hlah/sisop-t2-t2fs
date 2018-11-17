b7dbe46cd34d862c0f53ac9e7833845d7456d499
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-11-17 00:00:09 +0000

    Corrigidos alguns erros de compilação

--- t2fs/src/delete2.c
+++ t2fs/src/delete2.c
@@ -1,16 +1,18 @@
 #include "t2fs_internal.h"
 #include "apidisk.h"
 
+#include <string.h>
+
 int delete2(char* filename) {
-    t2fs_init();
+	t2fs_init();
 
     FILE2 handle;
     int i;
-    unsigned int sector_buffer [SECTOR_SIZE / 4];
+	unsigned int sector_buffer [SECTOR_SIZE / 4];
     unsigned int sector;
     unsigned int sector_offset;
     void* cluster_data;
-    unsigned int cluster_registry = 0;
+	unsigned int cluster_registry = 0;
     // struct t2fs_record record;
 
     /*
@@ -38,7 +40,7 @@ int delete2(char* filename) {
     if (t2fs_open_files[handle].file_record->TypeVal != TYPEVAL_REGULAR)
         return -1;
 
-    // verifica se o arquivo já está aberto com outro handle. Se já está, outro handle é fechado
+    // verifica se o arquivo já está aberto com outro handle. Se já está, arquivo é fechado
     for (i = 0; i < MAXIMUM_OPEN_FILES; i++)
         if (t2fs_open_files[handle].file_record->name == t2fs_open_files[i].file_record->name && t2fs_open_files[handle].dir_cluster == t2fs_open_files[i].dir_cluster && handle != i)
             close2(i + 1);
@@ -57,16 +59,16 @@ int delete2(char* filename) {
         return -1;
 
     // procura registro do arquivo no seu diretório
-    cluster_data = t2fs_read_cluster( t2fs_open_files[handle].dir_cluster );
-    while( strcmp(((struct t2fs_record*)cluster_data+cluster_registry)->name, t2fs_open_files[handle].file_record->name) != 0) {
-        cluster_registry++;
-    }
+	cluster_data = t2fs_read_cluster( t2fs_open_files[handle].dir_cluster );
+	while( strcmp(((struct t2fs_record*)cluster_data+cluster_registry)->name, t2fs_open_files[handle].file_record->name) != 0) {
+		cluster_registry++;
+	}
 
-    // libera registro e escreve no disco
+	// libera registro e escreve no disco
     ((struct t2fs_record*)cluster_data+cluster_registry)->TypeVal = TYPEVAL_INVALIDO;
-    if (t2fs_write_cluster(t2fs_open_files[handle].dir_cluster, cluster_data) != 0)
+	if (t2fs_write_cluster(t2fs_open_files[handle].dir_cluster, cluster_data) != 0)
         return -1;
     free(cluster_data);
 
-    return 0;
+	return 0;
 }
